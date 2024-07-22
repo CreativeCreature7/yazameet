405b090c1ed2961ac55a56845dc67c2a15603e1d
Guy Ben-Hemo <guybh200211@gmail.com>
2024-07-23 01:42:05 +0300

    fix login

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -7,9 +7,11 @@ import {
 } from "@/components/ui/card";
 import { getTranslations } from "next-intl/server";
 import { LoginForm } from "@/app/_components/login-form";
+import { getProviders } from "next-auth/react";
 
 export default async function Login() {
   const t = await getTranslations();
+  const providers = await getProviders();
 
   return (
     <Card className="fixed left-1/2 top-1/2 mx-auto max-w-sm -translate-x-1/2 -translate-y-1/2">
@@ -18,7 +20,7 @@ export default async function Login() {
         <CardDescription>{t("enter_your_email_below")}</CardDescription>
       </CardHeader>
       <CardContent>
-        <LoginForm />
+        <LoginForm providers={providers} />
       </CardContent>
     </Card>
   );
