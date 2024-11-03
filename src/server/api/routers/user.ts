27a3ffeb571fd0ec86fb790ce20fa8742c583010
Guy Ben-Hemo <guybh200211@gmail.com>
2024-11-04 00:57:33 +0200

    add edit profile

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -49,4 +49,38 @@ export const userRouter = createTRPCRouter({
       },
     });
   }),
+
+  getCurrentUser: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.user.findUnique({
+      where: { id: ctx.session.user.id },
+      select: {
+        name: true,
+        image: true,
+        roles: true,
+        year: true,
+      },
+    });
+  }),
+  updateDetails: protectedProcedure
+    .input(
+      z.object({
+        fullName: z.string().min(1),
+        profilePicture: z.string().min(1),
+        year: z.nativeEnum(Year),
+        roles: z.array(z.nativeEnum(Roles)).min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          name: input.fullName,
+          image: input.profilePicture,
+          roles: input.roles,
+          year: input.year,
+        },
+      });
+    }),
 });
